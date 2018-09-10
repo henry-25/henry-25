9de5d9d0a3b7b20dd4d6b5d62d0a463f2c93cd5a
henry-25 <henry.r.welsh@gmail.com>
2018-09-10 11:27:06 -0400

    Using jquery for button clicks

--- js/index.js
+++ js/index.js
@@ -8,17 +8,34 @@ var colorArray = ['#FF6633', '#FFB399', '#FF33FF', '#FFFF99', '#00B3E6',
 		  '#4D8066', '#809980', '#E6FF80', '#1AFF33', '#999933',
 		  '#FF3380', '#CCCC00', '#66E64D', '#4D80CC', '#9900B3', 
 		  '#E64D66', '#4DB380', '#FF4D4D', '#99E6E6', '#6666FF'];
-
-$('#nextPageRow').hide();
 var canvas = document.getElementById("myCanvas");
 var ctx = canvas.getContext("2d");
+var ballColor;
 var ballRadius = 10;
 var x = canvas.width/2;
 var y = canvas.height-30;
 var dx = 2;
 var dy = -2;
-var ballColor;
 var shape = colorBall;
+
+$(document).ready(function () {
+	$('#nextPageRow').hide();
+	$('#changeBallColorClick').click(function() {
+		changeBallColor();
+	})
+	$('#changeBallDirectionClick').click(function() {
+		changeBallDirection();
+	})
+	$('#randomizeBallDirectionClick').click(function() {
+		randomizeBallDirection();
+	})
+	$('#stopBallClick').click(function() {
+		stopBall();
+	})
+	$('#startBallClick').click(function() {
+		startBall();
+	})
+});
 
 function colorBall(color) {
   ctx.beginPath();
